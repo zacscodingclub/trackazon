507ba6dd65e8e4fd06ce37b0f235c8da47dfa641
zacscodingclub <zbaston@gmail.com>
2016-08-21 09:44:51 -0500

    add strong params for my User fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  end
 end
