30a2f7766269b6fac1fff702cd6d0d5ba2c7a0ed
zacscodingclub <zbaston@gmail.com>
2016-08-21 08:42:30 -0500

    add User name validations and instance methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  validate :first_name, presence: true
+  validate :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable, :validatable
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
