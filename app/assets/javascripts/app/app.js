ea9df3f035e3ab1c5d9e439dba0a5429787bb70b
zacscodingclub <zbaston@gmail.com>
2016-08-21 10:26:00 -0500

    working on angular app setup

---
+++ app/assets/javascripts/app/app.js
@@ -0,0 +1,18 @@
+angular
+    .module('app', []);
+
+
+    // ['ui.router'])
+    // .config(function ($stateProvider) {
+    //   $stateProvider
+    //     .state('inventory', {
+    //       url: '/inventory',
+    //       templateUrl: 'inventory.html',
+    //       controller: 'InventoryController as inv',
+    //       resolve: {
+    //         user: function($http, $stateParams) {
+    //           return $http.get('http://0.0.0.0:8882/rest/user/' + $stateParams.id)
+    //         }
+    //       }
+    //     })
+    // });
