6d78c3632411b85bc257830e2e1c46b8b48fe6f6
zacscodingclub <zbaston@gmail.com>
2016-09-06 09:48:35 -0400

    update GUI to allow user to only see in stock or out of stock items

--- app/assets/javascripts/app/controllers/InventoryController.js
+++ app/assets/javascripts/app/controllers/InventoryController.js
@@ -17,6 +17,38 @@ function InventoryController($scope, inventory, ProductService) {
         console.log("Error occurred: " + error);
       });
   }
+
+  ctrl.inStockItems = function() {
+    var inStock = [];
+
+    ProductService.getProducts().then(function(response) {
+      response.data.forEach(function(product) {
+        if (product.quantity !== product.quantity_sold) {
+          inStock.push(product)
+        }
+        })
+      }, function(error) {
+        console.log("Error occurred: " + error);
+      });
+
+    ctrl.products = inStock;
+  }
+
+  ctrl.outOfStockItems = function() {
+    var outOfStock = [];
+
+    ProductService.getProducts().then(function(response) {
+      response.data.forEach(function(product) {
+        if (product.quantity === product.quantity_sold) {
+          outOfStock.push(product);
+        }
+      })
+    }, function(error) {
+      console.log("Error occurred: " + error);
+    })
+
+    ctrl.products = outOfStock;
+  }
 }
 
 angular
